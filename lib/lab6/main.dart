df9d51ea316c6d59626e304fe0d389ec4c8fc84f
nmphi <minhphi03@gmail.com>
2024-09-23 22:36:05 +0700

    lab5

---
+++ lib/lab6/main.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'package:i_am_rich/lab6/screens/chat_screen.dart';
+import 'package:i_am_rich/lab6/screens/login_screen.dart';
+import 'package:i_am_rich/lab6/screens/registration_screen.dart';
+import 'package:i_am_rich/lab6/screens/welcome_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(FlashChat());
+}
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+      },
+    );
+  }
+}
